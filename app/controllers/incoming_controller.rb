5a6f8aaccfbe1964d1fa3180b7980f601a17d243
guy cao <guycao@gmail.com>
2014-05-27 15:28:27 -0700

    added simple tagging with ActsAsTaggableOn

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,15 +1,16 @@
 class  IncomingController < ApplicationController
 
-  # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
   skip_before_filter :verify_authenticity_token, only: [:create]
 
   def create
-    # puts "INCOMING PARAMS HERE: #{params}"
-
     @user = User.find_by_email(params[:sender])
     @url = params[:'body-plain'] 
     @hashtag = params[:subject] 
 
+    bookmark = Bookmark.new
+    bookmark.url = @url 
+    bookmark.tag_list.add(@hashtag)
+    bookmark.save
 
     head 200
   end
