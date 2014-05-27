2e79331641e6190affd79dbaca2f41703275fadc
Manuel Garcia <manuel.garcia@bfa.org>
2014-05-27 13:20:09 -0400

    fix routes back to how they were

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'welcome#index'
 
-
+#   devise_scope :user do
+#     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+#   end
 end
